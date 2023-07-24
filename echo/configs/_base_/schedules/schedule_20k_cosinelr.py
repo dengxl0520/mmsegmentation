e6798bc81d56b5d35282995e37de7dc122f8432c
DengXL <dengxl0520@gmail.com>
2023-07-25 00:48:44 +0800

    [add] configs

--- echo/configs/_base_/schedules/schedule_20k_cosinelr.py
+++ echo/configs/_base_/schedules/schedule_20k_cosinelr.py
@@ -1,6 +1,19 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optim_wrapper = dict(type='AmpOptimWrapper', optimizer=optimizer, clip_grad=None)
+embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
+optimizer = dict(
+    type='AdamW', lr=0.0001, weight_decay=0.05, eps=1e-8, betas=(0.9, 0.999))
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    optimizer=optimizer,
+    clip_grad=dict(max_norm=0.01, norm_type=2),
+    paramwise_cfg=dict(
+        custom_keys={
+            'backbone': dict(lr_mult=0.1, decay_mult=1.0),
+            'query_embed': embed_multi,
+            'query_feat': embed_multi,
+            'level_embed': embed_multi,
+        },
+        norm_decay_mult=0.0))
 # learning policy
 param_scheduler = [
     dict(type='LinearLR',
@@ -20,7 +33,7 @@ val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', log_metric_by_epoch=False, interval=20),
+    logger=dict(type='LoggerHook', log_metric_by_epoch=False, interval=50),
     param_scheduler=dict(type='ParamSchedulerHook'),
     checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000, max_keep_ckpts=5),
     sampler_seed=dict(type='DistSamplerSeedHook'))
---
+++ echo/configs/stecho/optimizer-SDG/stechonet_r50_sta_stechohead_20k_camus_1e-4.py
@@ -0,0 +1,4 @@
+_base_ = '../stechonet_r50_sta_stechohead_20k_camus.py'
+
+optimizer = dict( type='SDG', lr=0.0001)
+optim_wrapper = dict(optimizer=optimizer)
